9f4cd9ec2cd983d4ca9804f403402d3b7cb86bfb
KhalidIrfani <khalidirfani1234@gmail.com>
2023-09-06 01:19:53 +0500

    forth

--- quzeto/src/server.js
+++ quzeto/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://quzeto.vercel.app/"
+export const server = "https://quzeto.vercel.app"
 
-export const backend_url = "https://quzeto.vercel.app/"
+export const backend_url = "https://quzeto.vercel.app"
