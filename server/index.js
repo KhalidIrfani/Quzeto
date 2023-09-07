041d0d7b14afcd644541769b89b2e5b8ee613669
KhalidIrfani <khalidirfani1234@gmail.com>
2023-09-07 12:05:42 +0500

    fixes

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: 'http://localhost:3000',
+        origin: 'https://quzeto-7dci.vercel.app',
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD'],
     })
